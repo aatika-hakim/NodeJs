a37506bc8dd11817c63b640ed1a444db9fccd95e
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-04 21:04:27 +0500

    modules

---
+++ index.js
@@ -0,0 +1,11 @@
+// core modules 
+    // Global module
+    // no need to import
+
+    console.log("Core Modules");
+
+    // Non Global module
+    // which we need to import
+
+    const fs = require('fs');
+    fs.writeFileSync("app.js","New file");
